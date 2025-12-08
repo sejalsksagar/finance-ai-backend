be9d03b0def3d6118337bae74a6919fba432fd9d
Sejal Kshirsagar <sejalsksagar@gmail.com>
2025-12-08 21:33:32 +0530

    Initial backend setup

---
+++ src/main/java/com/finance/FinanceAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceAiBackendApplication.class, args);
+	}
+
+}
